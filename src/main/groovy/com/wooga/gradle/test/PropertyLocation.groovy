6d696058b8a34cfc00ebb71cf0b5a91a32920919
Christian Sagel <christian.sagel@wooga.net>
2022-06-21 09:58:13 +0200

    Add a properties command line flag location (#20)

--- src/main/groovy/com/wooga/gradle/test/PropertyLocation.groovy
+++ src/main/groovy/com/wooga/gradle/test/PropertyLocation.groovy
@@ -12,8 +12,14 @@ import static com.wooga.gradle.test.PropertyUtils.envNameFromProperty
  */
 abstract class PropertyLocation {
 
+    /**
+     * @return A description of the location
+     */
     abstract String reason()
-
+    /**
+     * @param args The set of arguments needed by the location to perform the write operation
+     * @return Optionally, a sequence of command line arguments to execute as part of the write operation (by the caller)
+     */
     abstract PropertyWrite write(PropertySetArguments args)
 
     @Override
@@ -21,11 +27,32 @@ abstract class PropertyLocation {
         return reason()
     }
 
+    /**
+     * No location (skipped)
+     */
     static PropertyLocation none = new NonePropertyLocation()
+    /**
+     * To the project's build.gradle script
+     */
     static PropertyLocation script = new BuildScriptPropertyLocation()
+    /**
+     * To the project's gradle.properties file
+     */
     static PropertyLocation property = new PropertiesFilePropertyLocation()
+    /**
+     * To the environment, before invoking the task
+     */
     static PropertyLocation environment = new EnvironmentPropertyLocation()
+    /**
+     * To the command line, as an argument in the task invocation
+     */
     static PropertyLocation commandLine = new CommandLineOptionPropertyLocation()
+
+    /**
+     * To the command line, as an argument in the task invocation using the -P flag.
+     * This location is not considered by default since it has the same function as {@code property}
+     */
+    static PropertyLocation propertyCommandLine = new PropertiesCommandLinePropertyLocation()
 
     static PropertyLocation valueOf(String name) {
         switch (name) {
@@ -102,6 +129,24 @@ class PropertiesFilePropertyLocation extends PropertyLocation {
 }
 
 /**
+ * Sets the property into by through the command line with the -P flag
+ */
+class PropertiesCommandLinePropertyLocation extends PropertyLocation {
+
+    @Override
+    String reason() {
+        "value is provided in properties by -P command line flag"
+    }
+
+    @Override
+    PropertyWrite write(PropertySetArguments args) {
+        def propValue = PropertyUtils.serializeValueToEnvironment(args.value, args.typeName, args.wrapFallback)
+        def commandLineArg = "-P${args.propertyKey ?: args.path}=${propValue}"
+        new PropertyWrite([commandLineArg])
+    }
+}
+
+/**
  * Sets the property onto the system environment
  */
 class EnvironmentPropertyLocation extends PropertyLocation {
@@ -125,9 +170,15 @@ class EnvironmentPropertyLocation extends PropertyLocation {
  */
 class CommandLineOptionPropertyLocation extends PropertyLocation {
 
+    String prefix
     String assignmentOperator
 
     CommandLineOptionPropertyLocation(String assigmentOperator = "=") {
+        this(null, assigmentOperator)
+    }
+
+    CommandLineOptionPropertyLocation(String prefix, String assigmentOperator) {
+        this.prefix = prefix
         this.assignmentOperator = assigmentOperator
     }
 
@@ -142,7 +193,12 @@ class CommandLineOptionPropertyLocation extends PropertyLocation {
             throw new MissingArgumentException("No command line option was provided for '${args.path}'")
         }
 
-        String option = args.commandLineOption
+        String option = ""
+        if (prefix != null) {
+            option += prefix
+        }
+        option += args.commandLineOption
+
         if (args.typeName != "Boolean" && args.value != Wildcard.INSTANCE) {
             def cliValue = PropertyUtils.wrapValueBasedOnType(args.value, args.typeName, args.wrapFallback)
             option += "${assignmentOperator}${cliValue}"
